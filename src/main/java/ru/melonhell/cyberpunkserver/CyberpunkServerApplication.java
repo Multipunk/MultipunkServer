096a533e142608fd8a81a733e19a06cbbfa71d43
MelonHell <MelonHell@ya.ru>
2022-02-25 18:03:56 +0300

    init

---
+++ src/main/java/ru/melonhell/cyberpunkserver/CyberpunkServerApplication.java
@@ -0,0 +1,13 @@
+package ru.melonhell.cyberpunkserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberpunkServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyberpunkServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/melonhell/cyberpunkserver/CyberpunkServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.melonhell.cyberpunkserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberpunkServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
